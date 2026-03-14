f3910a43a5165a6ccf696d32c9b0fb260ff51cec
Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>
2026-03-13 18:29:25 -0700

    Support feature toggles
    In some cases, we may want to enable/disable certain kind of tracers
    instead of enabling them all by default.
    
    Signed-off-by: Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,24 @@ async function run(): Promise<void> {
   try {
     logger.info(`Initializing ...`)
 
-    // Start step tracer
-    await stepTracer.start()
-    // Start stat collector
-    await statCollector.start()
-    // Start process tracer
-    await processTracer.start()
+    const enable_step_tracer = core.getInput('enable_step_tracer')
+    const enable_stat_collector = core.getInput('enable_stat_collector')
+    const enable_process_tracer = core.getInput('enable_process_tracer')
+
+    if (enable_step_tracer === 'true') {
+      // Start step tracer
+      await stepTracer.start()
+    }
+
+    if (enable_stat_collector === 'true') {
+      // Start stat collector
+      await statCollector.start()
+    }
+
+    if (enable_process_tracer === 'true') {
+      // Start process tracer
+      await processTracer.start()
+    }
 
     logger.info(`Initialization completed`)
   } catch (error: any) {
